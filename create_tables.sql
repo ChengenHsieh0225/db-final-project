1798543c09041e2cfb98ff08eba287677d4060e1
Chengen_Hsieh <wayne70813@gmail.com>
2024-05-26 02:39:22 +0800

    add channel to imput_image, add the procedure for conv2d

--- create_tables.sql
+++ create_tables.sql
@@ -1,98 +1,103 @@
-CREATE DATABASE final_project;
+CREATE DATABASE IF NOT EXISTS final_project;
 USE final_project;
 
 # input images
-CREATE TABLE input_image (
+CREATE TABLE IF NOT EXISTS input_image (
 	image_index INT,
     dim1 INT,
     dim2 INT,
+    channel INT,
     value FLOAT,
     CHECK (value >= 0 AND value <= 1)
 );
 
 # store the output of each layer
-CREATE TABLE conv2d_1_output (
+CREATE TABLE IF NOT EXISTS conv2d_1_output (
 	dim1 INT,
     dim2 INT,
     channel INT,
     value FLOAT
 );
-CREATE TABLE max_pooling_1_output (
+CREATE TABLE IF NOT EXISTS max_pooling_1_output (
 	dim1 INT,
     dim2 INT,
     channel INT,
     value FLOAT
 );
-CREATE TABLE conv2d_2_output (
+CREATE TABLE IF NOT EXISTS conv2d_2_output (
 	dim1 INT,
     dim2 INT,
     channel INT,
     value FLOAT
 );
-CREATE TABLE max_pooling_2_output (
+CREATE TABLE IF NOT EXISTS max_pooling_2_output (
 	dim1 INT,
     dim2 INT,
     channel INT,
     value FLOAT
 );
-CREATE TABLE flatten_output (
+CREATE TABLE IF NOT EXISTS flatten_output (
 	dim1 INT,
     value FLOAT
 );
-CREATE TABLE dense_1_output (
+CREATE TABLE IF NOT EXISTS dense_1_output (
 	dim1 INT,
     value FLOAT
 );
-CREATE TABLE dense_2_output (
+CREATE TABLE IF NOT EXISTS dense_2_output (
 	dim1 INT,
     value FLOAT
 );
 
 # store the weights for each layer
-CREATE TABLE conv2d_1_weights (
+CREATE TABLE IF NOT EXISTS conv2d_1_weights (
 	filter_index INT,
     dim1 INT,
     dim2 INT,
-    weight FLOAT
+    weight FLOAT,
+    PRIMARY KEY (filter_index, dim1, dim2)
 );
-CREATE TABLE conv2d_2_weights (
+CREATE TABLE IF NOT EXISTS conv2d_2_weights (
 	filter_index INT,
     dim1 INT,
     dim2 INT,
     channel INT,
-    weight FLOAT
+    weight FLOAT,
+    PRIMARY KEY (filter_index, dim1, dim2, channel)
 );
-CREATE TABLE dense_1_weights (
+CREATE TABLE IF NOT EXISTS dense_1_weights (
 	filter_index INT,
 	dim1 INT,
-    weight FLOAT
+    weight FLOAT,
+    PRIMARY KEY (filter_index, dim1)
 );
-CREATE TABLE dense_2_weights (
+CREATE TABLE IF NOT EXISTS dense_2_weights (
 	filter_index INT,
 	dim INT,
-    weight FLOAT
+    weight FLOAT,
+    PRIMARY KEY (filter_index, dim)
 );
 
 # store the biases for each layer
-CREATE TABLE conv2d_1_biases (
-	filter_index INT,
+CREATE TABLE IF NOT EXISTS conv2d_1_biases (
+	filter_index INT PRIMARY KEY,
     weight FLOAT
 );
-CREATE TABLE conv2d_2_biases (
-	filter_index INT,
+CREATE TABLE IF NOT EXISTS conv2d_2_biases (
+	filter_index INT PRIMARY KEY,
     weight FLOAT
 );
-CREATE TABLE dense_1_biases (
-	filter_index INT,
+CREATE TABLE IF NOT EXISTS dense_1_biases (
+	filter_index INT PRIMARY KEY,
     weight FLOAT
 );
-CREATE TABLE dense_2_biases (
-	filter_index INT,
+CREATE TABLE IF NOT EXISTS dense_2_biases (
+	filter_index INT PRIMARY KEY,
     weight FLOAT
 );
 
 # store the predictions of the current batch
-CREATE TABLE predictions (
+CREATE TABLE IF NOT EXISTS predictions (
 	data_index INT PRIMARY KEY,
     value1 FLOAT, value2 FLOAT, value3 FLOAT, value4 FLOAT, value5 FLOAT,
     value6 FLOAT, value7 FLOAT, value8 FLOAT, value9 FLOAT, value10 FLOAT
--- tensor_computation.sql
+++ tensor_computation.sql
@@ -1,20 +1,38 @@
-# conv2d_1
-
-# inner product
-SELECT SUM(I.value * W.weight) AS val 
-	FROM I, W
-    WHERE I.dim1=W.dim1 AND I.dim2=W.dim2;
-
+# inner product for conv2d
 delimiter //
-CREATE PROCEDURE inner_product (IN dim1_shift INT, dim2_shift INT, OUT RESULT INT)
+CREATE PROCEDURE inner_product_conv2d (IN dim1_shift INT, IN dim2_shift INT, IN I VARCHAR(50), IN W VARCHAR(50), OUT RESULT FLOAT)
 BEGIN
-	SELECT SUM(I.value * W.weight) INTO result 
-	FROM I, W
-    WHERE (I.dim1+dim1_shift)=W.dim1 AND (I.dim2+dim2_shift)=W.dim2;
+	DECLARE query VARCHAR(255);
+	SET query = 'SELECT SUM(' + I + '.value * ' + W + '.weight) INTO RESULT FROM ' + I + ', ' + W
+    + 'WHERE (' + I + '.dim1 + dim1_shift) = ' + W + '.dim1 AND (' + I + '.dim2 + dim2_shift) = ' + W + '.dim2 AND (' + I + '.channel = ' + W + '.channel)';
+    EXECUTE query;
 END //
 delimiter ;
 
-CALL inner_product(1, 2, @RESULT);
-SELECT @RESULT;
+# conv2d layer 1
+delimiter //
+CREATE PROCEDURE conv2d_1(IN dim1_size INT, IN dim2_size INT)
+BEGIN
+	SET dim1 = 0;
+	loop1: LOOP
+		IF dim1 < dim1_size THEN
+			SET dim2 = 0;
+            loop2: LOOP
+				IF dim2 < dim2_size THEN
+					CALL inner_product_conv2d(dim1, dim2, 'input_image', 'conv2d_1_weights', @RESULT);
+                    INSERT INTO conv2d_1_output VALUES (dim1, dim2, channel, RESULT);
+					SET dim2 = dim2 + 1;
+					ITERATE loop2;
+				END IF;
+                LEAVE loop2;
+			END LOOP loop2;
+            SET dim1 = dim1 + 1;
+			ITERATE loop1;
+		END IF;
+		LEAVE loop1;
+    END LOOP loop1;
+END //
+delimiter ;
+
 
 
