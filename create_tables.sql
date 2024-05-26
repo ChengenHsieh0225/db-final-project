c6730c2433163bebc5f958bd47c2531b42a1b249
Chengen_Hsieh <wayne70813@gmail.com>
2024-05-26 12:01:36 +0800

    update conv2d (seems work)

--- create_tables.sql
+++ create_tables.sql
@@ -10,6 +10,7 @@ CREATE TABLE IF NOT EXISTS input_image (
     value FLOAT,
     CHECK (value >= 0 AND value <= 1)
 );
+DROP TABLE input_image;
 
 # store the output of each layer
 CREATE TABLE IF NOT EXISTS conv2d_1_output (
@@ -56,7 +57,7 @@ CREATE TABLE IF NOT EXISTS conv2d_1_weights (
     dim2 INT,
     channel INT,
     weight FLOAT,
-    PRIMARY KEY (filter_index, dim1, dim2)
+    PRIMARY KEY (filter_index, dim1, dim2, channel)
 );
 DROP TABLE conv2d_1_weights;
 CREATE TABLE IF NOT EXISTS conv2d_2_weights (
--- tensor_computation.sql
+++ tensor_computation.sql
@@ -1,29 +1,38 @@
-# inner product for conv2d
+# ReLu
 delimiter //
-CREATE PROCEDURE inner_product_conv2d (IN dim1_shift INT, IN dim2_shift INT, IN I VARCHAR(50), IN W VARCHAR(50), OUT RESULT FLOAT)
+CREATE PROCEDURE relu (tableName VARCHAR(50))
 BEGIN
-	DECLARE query VARCHAR(1024);
-    SET query = CONCAT(
-        'SELECT SUM(', I, '.value * ', W, '.weight) INTO @result FROM ', I, ', ', W, 
-        ' WHERE (', I, '.dim1 + ', dim1_shift, ') = ', W, '.dim1 AND (', 
-        I, '.dim2 + ', dim2_shift, ') = ', W, '.dim2 AND ', I, '.channel = ', W, '.channel'
+    SET @query = CONCAT(
+		'DELETE FROM ', tableName, ' WHERE value < 0'
     );
-    #SET query = 'SELECT SUM(input_image.value * conv2d_1_weights.weight) FROM input_image, conv2d_1_weights WHERE input_image.dim1 = 1';
-	#SET query = 'SELECT SUM(' + I + '.value * ' + W + '.weight) INTO RESULT FROM ' + I + ', ' + W + 'WHERE (' + I + '.dim1 + dim1_shift) = ' + W + '.dim1 AND (' + I + '.dim2 + dim2_shift) = ' + W + '.dim2 AND (' + I + '.channel = ' + W + '.channel)';
     PREPARE stmt FROM @query;
     EXECUTE stmt;
 END //
 delimiter ;
 
-DROP PROCEDURE inner_product_conv2d;
-DROP PROCEDURE conv2d_1;
+
+
+# inner product for conv2d
+delimiter //
+CREATE PROCEDURE inner_product_conv2d (dim1_shift INT, dim2_shift INT, I VARCHAR(50), W VARCHAR(50), OUT result FLOAT)
+BEGIN
+    SET @query = CONCAT(
+        'SELECT SUM(', I, '.value * ', W, '.weight) INTO @result FROM ', I, ', ', W, 
+        ' WHERE (', I, '.dim1 + ', dim1_shift, ') = ', W, '.dim1 AND (', 
+        I, '.dim2 + ', dim2_shift, ') = ', W, '.dim2 AND ', I, '.channel = ', W, '.channel'
+    );
+    PREPARE stmt FROM @query;
+    EXECUTE stmt;
+    SET result = @result;
+END //
+delimiter ;
 
 # conv2d layer 1
 delimiter //
 CREATE PROCEDURE conv2d_1(IN dim1_size INT, IN dim2_size INT)
 BEGIN
 	DECLARE dim1, dim2 INT;
-    DECLARE result FLOAT;
+    SET @result = 0;
 	SET dim1 = 0;
 	loop1: LOOP
 		IF dim1 < dim1_size THEN
@@ -31,7 +40,7 @@ BEGIN
             loop2: LOOP
 				IF dim2 < dim2_size THEN
 					CALL inner_product_conv2d(dim1, dim2, 'input_image', 'conv2d_1_weights', @RESULT);
-                    INSERT INTO conv2d_1_output VALUES (dim1, dim2, channel, RESULT);
+                    INSERT INTO conv2d_1_output VALUES (dim1, dim2, channel, @RESULT);
 					SET dim2 = dim2 + 1;
 					ITERATE loop2;
 				END IF;
@@ -46,7 +55,19 @@ END //
 delimiter ;
 
 
-CALL conv2d_1(28, 28);
 
 
+# for debug
+
+SET SQL_SAFE_UPDATES=0;
+DELETE FROM conv2d_1_weights WHERE weight < 0;
+CALL conv2d_1(3, 3);
+
+DROP PROCEDURE inner_product_conv2d;
+DROP PROCEDURE conv2d_1;
+
+CALL inner_product_conv2d(0, 0, 'input_image', 'conv2d_1_weights', @RESULT);
+
+SELECT * FROM conv2d_1_output;
+DELETE FROM conv2d_1_output;
 
